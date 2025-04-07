f7f8a61a7b86c641ff7d23f32d407c17d099c057
lTrachta <ltrachta@mi.unc.edu.ar>
2025-04-07 15:47:14 +0000

    estructura completa y test de funcionamiento

--- python/gini_api.py
+++ python/gini_api.py
@@ -1,6 +1,6 @@
 import requests
 import subprocess
-API_URL = "https://api.worldbank.org/v2/en/country/all/indicator/SI.POV.GINI?format=json&date=2011:2020&per_page=32500&page=1&country=Argentina"
+API_URL = "https://api.worldbank.org/v2/en/country/all/indicator/SI.POV.GINI?format=json&date=2011:2020&per_page=32500&page=1&country=ARG"
 
 def obtener_gini():
     res = requests.get(API_URL)
@@ -19,5 +19,5 @@ if __name__ == "__main__":
     print("Índice GINI obtenido:", valor_gini)
 
     # Ejecutar el programa en C y pasarle el valor como argumento
-    result = subprocess.run(["./programa_c", str(valor_gini)], capture_output=True, text=True)
+    result = subprocess.run(["../src/programa_c", str(valor_gini)], capture_output=True, text=True)
     print("Resultado de conversión:", result.stdout)
